e116692ce9a466ee1f56c48bdd10a07464a28a90
jdab2020 <jdabalos90@gmail.com>
2020-02-23 23:40:12 -0800

    added modules and posting api

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 // Dependencies
 // =============================================================
-var express = require("express");
-var path = require("path");
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const savedNotes = require("./db/db.json");
 
 // Sets up the Express App
 // =============================================================
-var app = express();
-var PORT = 3000;
+const app = express();
+const PORT = 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -22,6 +24,32 @@ app.get("/", function (req, res) {
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
+
+// Display all notes
+app.get("/api/notes", function(req,res){
+    res.json(savedNotes);
+});
+
+// Posting new notes
+app.post("/api/notes", function(req,res){
+    let newNote = req.body;
+    // Give an id for deleting
+    let id = savedNotes.length;
+    newNote.id = id;
+    console.log(newNote);
+    saveToDB(newNote);
+    res.json(newNote);
+});
+
+// Saving into database
+const saveToDB = (note) => {
+    fs.writeFileSync("./db/db.json",note, (err) => {
+        if (err) return console.log(err);
+    })
+}
+
+// Deleting notes
+
 
 // Starts the server to begin listening
 // =============================================================
